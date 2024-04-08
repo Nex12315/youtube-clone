736c204f919fb241a2666f4a60c35b226dd3f70d
Nex12315 <nikita20021804@gmail.com>
2024-04-09 01:51:13 +0300

    pomogite

---
+++ yt-web-client/app/firebase/firebase.ts
@@ -0,0 +1,48 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  User,
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb-W-vqlCYFcW-UyYNnoph1a6MueBROLk",
+  authDomain: "yt-clone-b13be.firebaseapp.com",
+  projectId: "yt-clone-b13be",
+  appId: "1:956268923930:web:bafd640318ce105700d5a3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the suer's credentials.
+ */
+export function signInWithGoogle() {
+  return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs the user out.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export function signOut() {
+  return auth.signOut();
+}
+
+/**
+ * Trigger a callback whe user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+export function onAuthStateChangedHelper(
+  callback: (user: User | null) => void
+) {
+  return onAuthStateChanged(auth, callback);
+}
